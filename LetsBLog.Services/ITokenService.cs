a2e7f320fa4b11bc126f776ca25080659224a6fd
Farhan Patel <farhanp9876@gmail.com>
2021-09-07 21:00:16 +0530

    Token Service Added. Services Project Completed.

---
+++ LetsBLog.Services/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetsBlog.Models.Account;
+
+namespace LetsBLog.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
