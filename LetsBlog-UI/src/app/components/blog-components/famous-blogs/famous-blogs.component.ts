d9bba4092f1c8e00934e8f3a247986158d283150
Farhan Patel <farhanp9876@gmail.com>
2021-09-21 22:17:21 +0530

    Famous Blogs Component created.

--- LetsBlog-UI/src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
+++ LetsBlog-UI/src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from 'src/app/models/blog/blog.model';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
-  selector: 'app-famous-blogs',
-  templateUrl: './famous-blogs.component.html',
-  styleUrls: ['./famous-blogs.component.scss']
+    selector: 'app-famous-blogs',
+    templateUrl: './famous-blogs.component.html',
+    styleUrls: ['./famous-blogs.component.scss'],
 })
 export class FamousBlogsComponent implements OnInit {
+    famousBlogs: Blog[] = [];
 
-  constructor() { }
+    constructor(private blogService: BlogService) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.blogService.getMostFamous().subscribe((blogs) => {
+            this.famousBlogs = blogs;
+        });
+    }
 }
